9526b4ed3943a1673133a6767c10afcacba6e14a
Rose <870536460@qq.com>
2017-12-25 22:22:26 +0800

    finish map init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 游戏逻辑控制器
+/// </summary>
+public class GameManager : MonoBehaviour {
+
+    //游戏等级
+    public int level = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -4,34 +4,52 @@ using UnityEngine;
 /// <summary>
 /// 控制地图和游戏整体逻辑
 /// </summary>
-public class MapManager : MonoBehaviour {
+public class MapManager : MonoBehaviour
+{
 
     public GameObject[] outWallArray;
     public GameObject[] floorArray;
     public GameObject[] wallArray;
+    public GameObject[] foodArray;
+    public GameObject[] enemyArray;
+    public GameObject exitObj;
 
     public int rows;
     public int cols;
 
     private List<Vector2> positionList;
     private Transform map;
+    private GameManager gameManager;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Awake()
+    {
+        gameManager = this.GetComponent<GameManager>();
         InitMap();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     /// <summary>
     /// 初始化围墙 地板 障碍物 食物 敌人
     /// </summary>
     private void InitMap()
     {
-        #region 初始化围墙
+        //获取要放障碍和敌人等物体的屏幕坐标集合
+        positionList = new List<Vector2>();
+        for (int x = 2; x < cols - 2; x++)
+        {
+            for (int y = 2; y < rows - 2; y++)
+            {
+                positionList.Add(new Vector2(x, y));
+            }
+        }
+
+        //初始化围墙
         map = new GameObject("Map").transform;
         for (int x = 0; x < cols; x++)
         {
@@ -39,41 +57,54 @@ public class MapManager : MonoBehaviour {
             {
                 if (x == 0 || y == 0 || x == cols - 1 || y == rows - 1)
                 {
-                    int index = Random.Range(0, outWallArray.Length);
-                    GameObject go = GameObject.Instantiate(outWallArray[index], new Vector3(x, y, 0), Quaternion.identity);
-                    go.transform.SetParent(map);
+                    GameObject.Instantiate(GetRandomObject(outWallArray), new Vector2(x,y), Quaternion.identity).transform.SetParent(map);
                 }
                 else
                 {
-                    int index = Random.Range(0, floorArray.Length);
-                    GameObject go = GameObject.Instantiate(floorArray[index], new Vector3(x, y, 0), Quaternion.identity);
-                    go.transform.SetParent(map);
+                    GameObject.Instantiate(GetRandomObject(floorArray), new Vector2(x, y), Quaternion.identity).transform.SetParent(map);
                 }
             }
         }
-        #endregion
 
-        #region 获取要放障碍和敌人等物体的屏幕坐标集合
-        positionList = new List<Vector2>();
-        for (int x = 2; x < cols-2; x++)
-        {
-            for (int y = 2; y < rows-2; y++)
-            {
-                positionList.Add(new Vector2(x,y));
-            }
-        }
-        #endregion
-
-        #region 初始化障碍物
+        //初始化障碍物
         int wallCount = Random.Range(2, 8);  //随机生成2-8个障碍
-        for (int i = 0; i < wallCount; i++)
+        InitComponent(wallArray, wallCount);
+
+        //初始化食物
+        int foodCount = Random.Range(0, gameManager.level * 2 + 1);
+        InitComponent(foodArray, foodCount);
+
+        //初始化敌人
+        int enemyCount = gameManager.level / 2 + 1;
+        InitComponent(enemyArray, enemyCount);
+
+        //初始化出口
+        GameObject.Instantiate(exitObj, new Vector2(cols-2, rows-2), Quaternion.identity).transform.SetParent(map);
+    }
+
+    //获取随机位置
+    private Vector2 GetRandomPosition()
+    {
+        int positionIndex = Random.Range(0, positionList.Count);
+        Vector2 pos = positionList[positionIndex];
+        positionList.RemoveAt(positionIndex);
+        return pos;
+    }
+
+    //获取随机对象
+    private GameObject GetRandomObject(GameObject[] objectPrefabs)
+    {
+        int index = Random.Range(0, objectPrefabs.Length);
+        return objectPrefabs[index];
+    }
+
+    //初始化游戏组件,并设置父组件为map
+    //objArray:要初始化的地图资源
+    private void InitComponent(GameObject[] objArray,int count)
+    {
+        for (int i = 0; i < count; i++)
         {
-            int positionIndex = Random.Range(0,positionList.Count);
-            Vector2 position = positionList[positionIndex];
-            int wallIndex = Random.Range(0, wallArray.Length);
-            GameObject.Instantiate(wallArray[wallIndex], position, Quaternion.identity).transform.SetParent(map);
+            GameObject.Instantiate(GetRandomObject(objArray), GetRandomPosition(), Quaternion.identity).transform.SetParent(map);
         }
-        
-        #endregion
     }
 }
